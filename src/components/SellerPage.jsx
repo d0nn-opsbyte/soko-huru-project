e1ed3d2ce6a94e65c5ad702e1b8500f1f2596144
Rodney_Karani <rodneykarani07@gmail.com>
2025-07-23 02:15:02 +0300

    Add sellers content

---
+++ src/components/SellerPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BuyerPage = () => {
+  return (
+    <div>
+      <h1>Buyer Page</h1>
+      <p>This where buyers will see items for sale.</p>
+        {/*Add Buyer-specific navbar and content here*/}
+    </div>
+  );
+};
+
+export default BuyerPage;
