4cf2b1aa677c43cc3ae738539d7f4fd67372407a
Rodney_Karani <rodneykarani07@gmail.com>
2025-07-23 00:52:49 +0300

    Add components and app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import BuyerPage from "./components/BuyerPage";
+import SellerPage from "./components/SellerPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/buyer" element={<BuyerPage />} />
+        <Route path="/seller" element={<SellerPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
