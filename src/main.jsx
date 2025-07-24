05164d208b47854eabd271eb00b9a245281701e1
Denniskaranja2023! <denniswanyeki2021@gmail.com>
2025-07-24 10:17:54 +0300

    change router structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import routes from './Route'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
 
-const router = createBrowserRouter(routes);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
